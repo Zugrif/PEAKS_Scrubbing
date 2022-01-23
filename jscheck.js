8fdbafa6da3c712d5dc810a831db7b9011ac8474
Zugrif <101584742+Zugrif@users.noreply.github.com>
2022-01-23 13:39:27 +0000

    Comment formatting

--- jscheck.js
+++ jscheck.js
@@ -13,3 +13,5 @@
     E, EPSILON, PI, any, args, array, boolean, cases, charCodeAt, character,
     check, claim, class, classification, classifier, codePointAt, concat,
     detail, fail, falsy, fill, findIndex, floor, forEach, freeze, fromCodePoint,
+    integer, isArray, isSafeInteger, join, key, keys, length, literal, losses,
+    lost, map, name, nr_trials, number, object, ok, pass, predicate, push,
