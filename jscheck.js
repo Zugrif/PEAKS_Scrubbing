3ab0b94fa039ba4a031f64dfb1d4cc18eaae12b2
Zugrif <101584742+Zugrif@users.noreply.github.com>
2022-08-10 13:08:51 +0000

    Prepare 0.4.5 release

--- jscheck.js
+++ jscheck.js
@@ -334,3 +334,5 @@ function crunch(detail, cases, serials) {
                 {
                     type,
                     serial: the_case.serial,
+                    classification: the_case.classification,
+                    args: JSON.stringify(
